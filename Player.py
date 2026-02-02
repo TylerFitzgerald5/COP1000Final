d1ae65c6f99ae4ba47140bdf1064735d4b0aba67
Tyler Fitzgerald <tfitzgerald5@hawkmail.hccfl.edu>
2026-02-02 08:49:35 -0500

    Initial Commit

---
+++ Player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, name, team, elo):
+        self.name - name
+        #team of pokemon used by the player
+        self.team = team
+        self.elo = elo
---
+++ Pokemon.py
@@ -0,0 +1,48 @@
+import requests
+import Player
+
+#URL = input("Please input a Pokemon Showdown replay link: ")
+URL = "https://replay.pokemonshowdown.com/gen9ou-2526545993"
+log = requests.get(URL + ".log").text
+
+logList = log.split("\n")
+
+p1 = logList[0][4:]
+p1Elo = logList[0][-4:]
+p2 = logList[1][4:]
+p2Elo = logList[1][-4:]
+
+# PARSE ARRAY, FIND FIRST ENTRY INCLUDING "|player|p1" TO FIND PLAYER NAME AND ELO
+
+
+pokeNames1 = []
+pokeNames2 = []
+
+
+
+for i in logList:
+    if(i[0:8] == "|poke|p1"):
+        pokeNames1.append(i[9:])
+
+
+    if(i[0:8] == "|poke|p2"):
+        pokeNames2.append(i[9:])
+
+#Gets rid of pokemon genders and converts pokeName2 to only their names
+for i in range(len(pokeNames2)):
+    seperator = pokeNames2[i].find(',')
+    if seperator == -1:
+        seperator = pokeNames2[i].find('|')
+    pokeNames2[i] = pokeNames2[i][0:seperator]
+
+for i in range(len(pokeNames1)):
+    seperator = pokeNames1[i].find(',')
+    if seperator == -1:
+        seperator = pokeNames1[i].find('|')
+    pokeNames1[i] = pokeNames1[i][0:seperator]
+
+#player1 = Player(p1, )
+print(p1Elo)
+print("\n")
+print(p2Elo)
+
