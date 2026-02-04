be8b66b95311de37e972145afae3cc18dae7c4b6
Tyler Fitzgerald <tfitzgerald5@hawkmail.hccfl.edu>
2026-02-04 08:32:46 -0500

    util.py comments

--- Util.py
+++ Util.py
@@ -1,13 +1,18 @@
-def PCutBefore(input, reps = 1):
+##These are Utility functions used in pokemon.py
+##They are for altering string values to extract important information from lines
+
+
+
+#Pipe cuts and returns everything after the pipe
+#Can be called multiple times using reps. Defaults to 1
+def PCutAfter(input, reps = 1):
     output = input
     for i in range(reps):
         index = output.find("|") + 1
         output = output[index:]
     return output
 
-
-def PCutAfter(input):
+#Pipe cuts and returns everything before the pipe
+def PCutBefore(input):
     index = input.find("|")
     return input[:index]
-
-print(PCutAfter("player|p1|buhuizhibuyutianti|blackbelt-gen4dp|1159"))
