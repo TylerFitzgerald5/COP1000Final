53b9b422ce5f5062e0814b17f35c4bf5c464f521
Tyler Fitzgerald <tfitzgerald5@hawkmail.hccfl.edu>
2026-03-11 09:16:52 -0400

    started data parser and make user search

--- dataParser.py
+++ dataParser.py
@@ -1,1 +1,59 @@
-print("This is a dataparser")
+import sqlite3
+from sqlite3 import Error
+
+
+
+
+
+
+
+
+
+
+path = "database.db"
+connection = None
+try:
+    connection = sqlite3.connect(path)
+    print("Connected to Database")
+except Error as e:
+    print("The error " + e + " occured")
+
+cursor = connection.cursor()
+
+
+
+def userSearch(username):
+    sql = "SELECT * FROM players WHERE username = ?"
+    nameTuple = (username,)
+    cursor.execute(sql, nameTuple)
+    print(cursor.fetchall())
+
+
+
+
+def Search():
+    print("What do you want to search for?")
+    print("1: user search")
+    print("2: pokemon usage rate")
+    print("3: pokemon top usage list")
+    inputNum = input("Your Decision: ")
+
+
+    match inputNum:
+        case "1":
+            name = input("Please enter a username: ")
+            userSearch(name)
+        case "2":
+            print("You entered 2")
+        case "3":
+            print("You entered 3")
+        case _:
+            print("Please input a valid number")
+
+
+
+
+
+
+
+Search()
